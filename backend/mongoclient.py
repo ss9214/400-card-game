70dbca5c573b49aa4ef6c301212620a716688152
Srihari Srivatsa <109015229+ss9214@users.noreply.github.com>
2024-05-20 00:47:28 -0400

    Update mongoclient.py

--- backend/mongoclient.py
+++ backend/mongoclient.py
@@ -1,6 +1,6 @@
 from pymongo import MongoClient
 
-client = MongoClient("mongodb+srv://srihari21:Minisrij%40921@cluster0.lpxpmgs.mongodb.net/?retryWrites=true&w=majority")
+client = MongoClient("mongodb+srv://username:password@cluster0.lpxpmgs.mongodb.net/?retryWrites=true&w=majority")
 db = client["400Data"]
 users = db["Users"]
 games = db["Games"]
