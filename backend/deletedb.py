16824267ba5d5f9f936bbc8e726e20cd50c42264
Srihari Srivatsa <iamsrihari.921@gmail.com>
2025-05-20 22:41:07 -0400

    restarting game with different stack

--- backend/deletedb.py
+++
@@ -1,4 +0,0 @@
-from mongoclient import users,games
-
-users.delete_many({})
-games.delete_many({})
--- backend/mongoclient.py
+++
@@ -1,6 +0,0 @@
-from pymongo import MongoClient
-
-client = MongoClient("mongodb+srv://username:password@cluster0.lpxpmgs.mongodb.net/?retryWrites=true&w=majority")
-db = client["400Data"]
-users = db["Users"]
-games = db["Games"]
