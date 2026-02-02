3510fec91562cc2f8824ccd29d65fbb3b814c350
Srihari Srivatsa <iamsrihari.921@gmail.com>
2026-02-02 12:08:17 -0500

    update db names for prod

--- backend/create-rooms-table.js
+++ backend/create-rooms-table.js
@@ -12,7 +12,7 @@ const client = new DynamoDBClient({
 
 async function createRoomsTable() {
   const params = {
-    TableName: process.env.DYNAMODB_ROOMS_TABLE || 'rooms_dev',
+    TableName: process.env.DYNAMODB_ROOMS_TABLE || 'rooms',
     KeySchema: [
       { AttributeName: 'code', KeyType: 'HASH' } // Partition key
     ],
